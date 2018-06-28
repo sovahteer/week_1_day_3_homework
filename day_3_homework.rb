0d949c4ea10065e31319244b7086e53d987e9376
Euan <thecableton@gmail.com>
2018-06-28 08:58:42 +0100

    completed_exercise_b

--- day_3_homework.rb
+++ day_3_homework.rb
@@ -1,45 +1,45 @@
-# # #Exercise A
-#
-# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston",
-#   "Haymarket" ]
-#
-#   # 1 put Edinburgh stop to end of array
-#   stops.push("Edinburgh Waverley")
-#
-#   #2 put Glasgow stop to start of array
-#   stops.unshift("Glasgow Queen Street")
-#
-#   #3 put Polmont between Falkirk and Linlithgow
-#   stops.insert(4, "Polmont")
-#
-#   #4 show index number for Linlithgow
-#   index = stops.index("Linlithgow")
-#
-#   #5 remove Livingston by name
-#   stops.delete("Livingston")
-#
-#   #6 removes Cumbernauld from array
-#   stops.slice!(2)
-#
-#   #7 returns number of entries in array
-#   count = stops.count
-#
-#   #8 returns stops in reverse
-#   reverse = stops.reverse
-#
-#   #9 How many ways can we return "Falkirk High" from the array?
-#   stops[3]
-#
-#   stops.slice(3)
-#
-#   stops.fetch(3)
-#
-#   stops.at(3)
-#
-#   #10 prints stops as a for loop
-#   for stop in stops
-#     p stop
-#   end
+# #Exercise A
+
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston",
+  "Haymarket" ]
+
+  # 1 put Edinburgh stop to end of array
+  stops.push("Edinburgh Waverley")
+
+  #2 put Glasgow stop to start of array
+  stops.unshift("Glasgow Queen Street")
+
+  #3 put Polmont between Falkirk and Linlithgow
+  stops.insert(4, "Polmont")
+
+  #4 show index number for Linlithgow
+  index = stops.index("Linlithgow")
+
+  #5 remove Livingston by name
+  stops.delete("Livingston")
+
+  #6 removes Cumbernauld from array
+  stops.slice!(2)
+
+  #7 returns number of entries in array
+  count = stops.count
+
+  #8 returns stops in reverse
+  reverse = stops.reverse
+
+  #9 How many ways can we return "Falkirk High" from the array?
+  stops[3]
+
+  stops.slice(3)
+
+  stops.fetch(3)
+
+  stops.at(3)
+
+  #10 prints stops as a for loop
+  for stop in stops
+    p stop
+  end
 
 #Exercise B
 
@@ -116,7 +116,10 @@ p users ["Erik"][:pets][0][:species]
 p users ["Erik"][:lottery_numbers][2]
 
 #6
-p users ["Avril"][:lottery_numbers][0..3]
+ nums = users ["Avril"][:lottery_numbers]
+  if nums / 2 == 0
+    p nums
+  end
 
 #7
 p users ["Erik"][:lottery_numbers].push(7)
@@ -131,8 +134,4 @@ p users ["Erik"][:pets]<< {
 }
 
 #10
-p users << {
-  "Euan" => {
-    :home_town => "Prestonpans"
-  },
-}
+p users ["Euan"] = "new_user"
